298cf360dc26083a83119f16068f4323af266328
Light <ogatatsumi1152@gmail.com>
2025-05-16 03:37:04 +0700

    dumb fix

--- routes/characters.js
+++ routes/characters.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const constants = require('../data/constants');
 
-router.get('/characters', (req, res) => {
+router.get('/', (req, res) => {
   res.json({
     characters: constants.allowedCharacters,
     pets: constants.allowedPets,
