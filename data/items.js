17c9f583b674a95c9335356e3f1b76be5413616a
Light <ogatatsumi1152@gmail.com>
2025-05-17 07:18:57 +0700

    Minor changes

--- data/items.js
+++ data/items.js
@@ -1,10 +1,10 @@
 module.exports = {
     availableItems: [
-      { name: 'Small HP Potion', type: 'potion', price: 3, effect: 'heal', amount: 30 },
-      { name: 'Medium HP Potion', type: 'potion', price: 5, effect: 'heal', amount: 50 },
-      { name: 'Large HP Potion', type: 'potion', price: 7, effect: 'heal', amount: 100 },
-      { name: 'ATK Potion', type: 'statPotion', price: 175, effect: 'atk', amount: 4 },
-      { name: 'DEF Potion', type: 'statPotion', price: 175, effect: 'def', amount: 2 },
+      { name: 'Small HP Potion', type: 'potion', price: 25, effect: 'heal', amount: 35 },
+      { name: 'Medium HP Potion', type: 'potion', price: 50, effect: 'heal', amount: 80 },
+      { name: 'Large HP Potion', type: 'potion', price: 100, effect: 'heal', amount: 170 },
+      { name: 'ATK Potion', type: 'statPotion', price: 170, effect: 'atk', amount: 4 },
+      { name: 'DEF Potion', type: 'statPotion', price: 170, effect: 'def', amount: 2 },
     ]
   };
   
