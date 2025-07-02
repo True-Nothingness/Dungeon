f38f5b1695633673b9402791de7621f3b71c74e4
Light <ogatatsumi1152@gmail.com>
2025-07-02 17:58:39 +0700

    Fixed dumb mistake

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 const userRoutes = require('./routes/user');
 const taskRoutes = require('./routes/task');
